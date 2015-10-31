9ec0a503a43274b3d30628210fee9d55f26fbdd1
gingprint <gingprint@gmail.com>
2015-10-31 14:45:07 +0700

    loading dengan angular js

---
+++ app.js
@@ -0,0 +1,37 @@
+
+
+angular.module('LoadingBarExample', ['chieffancypants.loadingBar', 'ngAnimate'])
+  .config(function(cfpLoadingBarProvider) {
+    cfpLoadingBarProvider.includeSpinner = true;
+  })
+
+  .controller('ExampleCtrl', function ($scope, $http,$timeout,cfpLoadingBar) {
+    $scope.posts = [];
+   
+    $scope.fetch = function() {
+   
+      $http.get('data.json').success(function(data) {
+        $scope.posts = data;
+        console.log(data);
+      });
+    };
+
+    $scope.start = function() {
+      cfpLoadingBar.start();
+    };
+
+    $scope.complete = function () {
+      cfpLoadingBar.complete();
+    }
+
+
+    // fake the initial load so first time users can see it right away:
+    $scope.start();
+    $scope.fakeIntro = true;
+    $timeout(function() {
+      $scope.complete();
+      $scope.fakeIntro = false;
+    }, 10);
+
+
+  });
